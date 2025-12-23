9d3fd4e61c4ff415142435ee7cd4ea79eb8e96b2
Muhammad Haseeb Raza <haseebraza2004@gmail.com>
2025-12-23 19:46:04 +0500

    carousel adde from skipper ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,52 @@
 import { Button } from "@/components/ui/button";
+import { Carousel_005 } from "@/components/ui/skiper-ui/skiper51";
 
 export default function Home() {
+  const images = [
+    {
+      src: "/Bbq.jpg",
+      alt: "Description 1",
+    },
+    {
+      src: "/Biryani.jpg",
+      alt: "Description 2",
+    },
+    {
+      src: "/Burger2.jpg",
+      alt: "Description 3",
+    },
+    {
+      src: "/Wraps.jpg",
+      alt: "Description 4",
+    },
+  ];
+
   return (
-    <main className="bg-[] min-h-screen flex items-center justify-center">
-      <div className="text-center space-y-6 text-white">
-        <h1 className="text-5xl font-bold tracking-wide">
-          Welcome to Cafe Online
-        </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 relative">
+      <Carousel_005
+        className={"mt-20 min-h-screen"}
+        images={images}
+        showPagination={true}
+        loop={true}
+        autoplay={true}
+        spaceBetween={0}
+      />
 
-        <p className="text-white/80 max-w-md mx-auto">
-          Experience premium coffee and handcrafted flavors.
-        </p>
+      <main className="flex items-center justify-center">
+        <div className="text-center space-y-6 text-white">
+          <h1 className="text-5xl font-bold tracking-wide">
+            Welcome to Cafe Online
+          </h1>
 
-        <Button className="bg-yellow-500 text-black hover:bg-yellow-400">
-          Explore Menu
-        </Button>
-      </div>
-    </main>
+          <p className="text-white/80 max-w-md mx-auto">
+            Experience premium coffee and handcrafted flavors.
+          </p>
+
+          <Button className="bg-yellow-500 text-black hover:bg-yellow-400">
+            Explore Menu
+          </Button>
+        </div>
+      </main>
+    </div>
   );
 }
---
+++ src/components/ui/skiper-ui/skiper51.jsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import React from "react";
+import { Autoplay, EffectCreative, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-creative";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const Carousel_005 = ({
+  images,
+  className,
+  showPagination = false,
+  showNavigation = false,
+  loop = true,
+  autoplay = false,
+  spaceBetween = 0
+}) => {
+  const css = `
+  .Carousal_005 {
+    width: 100%;
+    height: 450px;
+    padding-bottom: 50px !important;
+  }
+  
+  .Carousal_005 .swiper-slide {
+    background-position: center;
+    background-size: cover;
+     border-radius: 25px;
+  }
+
+  .Carousal_005 .swiper-pagination-bullet {
+    background-color: #000 !important;
+  }
+ 
+  `;
+  return (
+    <motion.div
+      initial={{ opacity: 0, translateY: 20 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{
+        duration: 0.3,
+        delay: 0.5,
+      }}
+      className={cn("relative w-full max-w-4xl px-5", className)}>
+      <style>{css}</style>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="w-full">
+        <Swiper
+          spaceBetween={spaceBetween}
+          autoplay={
+            autoplay
+              ? {
+                delay: 1500,
+                disableOnInteraction: true,
+              }
+              : false
+          }
+          effect="creative"
+          grabCursor={true}
+          slidesPerView="auto"
+          centeredSlides={true}
+          loop={loop}
+          pagination={
+            showPagination
+              ? {
+                clickable: true,
+              }
+              : false
+          }
+          navigation={
+            showNavigation
+              ? {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              }
+              : false
+          }
+          className="Carousal_005"
+          creativeEffect={{
+            prev: {
+              shadow: true,
+              translate: [0, 0, -400],
+            },
+            next: {
+              translate: ["100%", 0, 0],
+            },
+          }}
+          modules={[EffectCreative, Pagination, Autoplay]}>
+          {images.map((image, index) => (
+            <SwiperSlide key={index} className="">
+              <Image
+                className=" scale-105 rounded-3xl object-cover"
+                src={image.src}
+                alt={image.alt}
+                fill={true}
+              />
+            </SwiperSlide>
+          ))}
+          {showNavigation && (
+            <div>
+              <div className="swiper-button-next after:hidden">
+                <ChevronRightIcon className="h-6 w-6 text-white" />
+              </div>
+              <div className="swiper-button-prev after:hidden">
+                <ChevronLeftIcon className="h-6 w-6 text-white" />
+              </div>
+            </div>
+          )}
+        </Swiper>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export { Carousel_005 };
